d333513f63a3b6cd6cc4cea3d424cb193194d74b
Oleg Gedzjuns <oleggedzjuns@gmail.com>
2022-07-18 12:00:16 +0300

    Removed unused code

--- index.js
+++ index.js
@@ -15,24 +15,12 @@ app.get('/pn.js', (_, res) => {
     res.sendFile(path.resolve('node_modules/playnetwork/dist/pn.js'));
 });
 
-const privateKey = fs.readFileSync('./ssl/localhost.key', 'utf8');
-const certificate = fs.readFileSync('./ssl/localhost.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+const key = fs.readFileSync('./ssl/localhost.key', 'utf8');
+const cert = fs.readFileSync('./ssl/localhost.crt', 'utf8');
+const credentials = { key, cert };
 
 const server = https.createServer(credentials, app);
-server.listen(parseInt(process.argv[2]) || 8080);
-
-pn.on('room:create', (from, data) => {
-    console.log('room:create', from, data);
-});
-
-pn.on('room:join', (from, data) => {
-    console.log('room:join', from, data);
-});
-
-pn.on('room:leave', (from, data) => {
-    console.log('room:leave', from, data);
-});
+server.listen(8080);
 
 await pn.start({
     redisUrl: process.env.REDIS_URL,
